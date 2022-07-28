2660b6181396321899abf27e74e0fa8cbfe1856e
HardTYZ <hardtyz@gmail.com>
2022-07-28 16:35:57 +0000

    Release v1.0.12

--- kube-prompt.rb
+++ kube-prompt.rb
@@ -1,14 +1,14 @@
 class KubePrompt < Formula
   desc "An interactive kubernetes client featuring auto-complete using go-prompt."
-  homepage "https://github.com/c-bata/kube-prompt"
-  version "1.0.11"
+  homepage "https://gitlab.com/zero_team/kube-prompt"
+  version "v1.0.12"
 
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
 
   if OS.mac?
-    url "https://zeroteam-tools.s3.eu-west-1.amazonaws.com/kube-prompt/kube-prompt_v1.0.11_darwin_#{arch}.zip"
+    url "https://zeroteam-tools.s3.eu-west-1.amazonaws.com/kube-prompt/kube-prompt_v1.0.12_darwin_#{arch}.zip"
   elsif OS.linux?
-    url "https://zeroteam-tools.s3.eu-west-1.amazonaws.com/kube-prompt/kube-prompt_v1.0.11_linux_#{arch}.zip"
+    url "https://zeroteam-tools.s3.eu-west-1.amazonaws.com/kube-prompt/kube-prompt_v1.0.12_linux_#{arch}.zip"
   end
 
   def install
